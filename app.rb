274e37f1d129d5cc3f14bc297b52a5fbf7322fe8
kanayannet <kanayannet@gmail.com>
2024-04-11 14:06:56 +0000

    一覧・投稿・更新 機能までつけた

--- app.rb
+++ app.rb
@@ -1,7 +1,26 @@
 require 'sinatra/base'
+require './lib/model'
 
 class BBS < Sinatra::Base
   get '/' do
-    'hello world'
+    @articles = Article.all.order(id: :desc)
+    erb :index
+  end
+
+  get '/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  post '/' do
+    art = params[:id].present? ?
+      Article.find_by(id: params[:id]) :
+      Article.new
+
+    art.name = params[:name]
+    art.subject = params[:subject]
+    art.body = params[:body]
+    art.save
+    redirect '/'
   end
 end
